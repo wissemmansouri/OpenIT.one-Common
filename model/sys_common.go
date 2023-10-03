2baddc1016232f8130401093868679f2ae8f660b
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-03 12:26:24 +0200

    Update sys_common.go

--- model/sys_common.go
+++ model/sys_common.go
@@ -3,5 +3,5 @@ package model
 type Result struct {
 	Success int         `json:"success" example:"200"`
 	Message string      `json:"message" example:"ok"`
-	Data    interface{} `json:"data" example:"返回结果"`
+	Data    interface{} `json:"data" example:"result"`
 }
