4c5b3dda893478a514dff2b74b5d5be7fcfeed3b
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-03 12:22:51 +0200

    Update device.go

--- model/device.go
+++ model/device.go
@@ -1,1 +1,13 @@
+package model
 
+type DeviceInfo struct {
+	LanIpv4     []string `json:"lan_ipv4"`
+	Port        int      `json:"port"`
+	DeviceName  string   `json:"device_name"`
+	DeviceModel string   `json:"device_model"`
+	Initialized bool     `json:"initialized"`
+	OS_Version  string   `json:"os_version"`
+	Hash        string   `json:"hash"`
+	RequestIp   string   `json:"request_ip,omitempty"`
+	TB_Ipv4     []string `json:"tb_ipv4"`
+}
