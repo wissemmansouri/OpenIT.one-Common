3b3cb3f3a815098d6f1cd50b39908efde73a6a5e
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-03 13:29:54 +0200

    Update port_test.go

--- utils/port/port_test.go
+++ utils/port/port_test.go
@@ -1,1 +1,29 @@
+package port_test
 
+import (
+	"fmt"
+	"testing"
+
+	"github.com/wissemmansouri/OpenIT.one-Common/utils/port"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPortAvailable(t *testing.T) {
+	//	fmt.Println(PortAvailable())
+	// fmt.Println(IsPortAvailable(6881,"tcp"))
+	p, _ := port.GetAvailablePort("udp")
+	fmt.Println("udp", p)
+	fmt.Println(port.IsPortAvailable(p, "udp"))
+
+	t1, _ := port.GetAvailablePort("tcp")
+	fmt.Println("tcp", t1)
+	fmt.Println(port.IsPortAvailable(t1, "tcp"))
+}
+
+func TestPorts(t *testing.T) {
+	tcpPorts, udpPorts, err := port.ListPortsInUse()
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, tcpPorts)
+	assert.NotEmpty(t, udpPorts)
+}
