eb3ca4c5297f5e9af4ea7588cf25f7393af54f19
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-03 12:30:02 +0200

    Update jwt_helper.go

--- utils/jwt/jwt_helper.go
+++ utils/jwt/jwt_helper.go
@@ -1,1 +1,131 @@
+package jwt
 
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"net/http"
+	"strconv"
+
+	"github.com/IceWhaleTech/OpenIT.one-Common/model"
+	"github.com/IceWhaleTech/OpenIT.one-Common/utils/common_err"
+	"github.com/gin-gonic/gin"
+)
+
+type JWK struct {
+	Kty string `json:"kty"`
+	Crv string `json:"crv"`
+	X   string `json:"x"`
+	Y   string `json:"y"`
+}
+
+type JWKS struct {
+	Keys []JWK `json:"keys"`
+}
+
+const JWKSPath = ".well-known/jwks.json"
+
+func ExceptLocalhost(publicKeyFunc func() (*ecdsa.PublicKey, error)) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.ClientIP() == "::1" || c.ClientIP() == "127.0.0.1" {
+			c.Next()
+			return
+		}
+
+		JWT(publicKeyFunc)(c)
+	}
+}
+
+func JWT(publicKeyFunc func() (*ecdsa.PublicKey, error)) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		if len(token) == 0 {
+			token = c.Query("token")
+		}
+
+		valid, claims, err := Validate(token, publicKeyFunc)
+		if err != nil || !valid {
+			message := "token is invalid"
+			c.JSON(http.StatusUnauthorized, model.Result{Success: common_err.ERROR_AUTH_TOKEN, Message: message})
+			c.Abort()
+			return
+		}
+
+		c.Request.Header.Add("user_id", strconv.Itoa(claims.ID))
+		c.Next()
+	}
+}
+
+func GenerateKeyPair() (*ecdsa.PrivateKey, *ecdsa.PublicKey, error) {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error generating key pair: %w", err)
+	}
+
+	publicKey := &privateKey.PublicKey
+
+	return privateKey, publicKey, nil
+}
+
+func GenerateJwksJSON(publicKey *ecdsa.PublicKey) ([]byte, error) {
+	jwk := JWK{
+		Kty: "EC",
+		Crv: "P-256",
+		X:   base64.RawURLEncoding.EncodeToString(publicKey.X.Bytes()),
+		Y:   base64.RawURLEncoding.EncodeToString(publicKey.Y.Bytes()),
+	}
+
+	jwks := JWKS{
+		Keys: []JWK{jwk},
+	}
+
+	return json.Marshal(jwks)
+}
+
+func PublicKeyFromJwksJSON(jwksJSON []byte) (*ecdsa.PublicKey, error) {
+	var jwks JWKS
+	err := json.Unmarshal(jwksJSON, &jwks)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling JWKS JSON: %w", err)
+	}
+
+	if len(jwks.Keys) == 0 {
+		return nil, fmt.Errorf("no keys in JWKS")
+	}
+
+	jwk := jwks.Keys[0]
+
+	x, err := base64.RawURLEncoding.DecodeString(jwk.X)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding X: %w", err)
+	}
+
+	y, err := base64.RawURLEncoding.DecodeString(jwk.Y)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding Y: %w", err)
+	}
+
+	publicKey := &ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     new(big.Int).SetBytes(x),
+		Y:     new(big.Int).SetBytes(y),
+	}
+
+	return publicKey, nil
+}
+
+func JWKSHandler(jwksJSON []byte) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write(jwksJSON)
+		if err != nil {
+			http.Error(w, "Error writing JWKS JSON", http.StatusInternalServerError)
+			return
+		}
+	})
+}
