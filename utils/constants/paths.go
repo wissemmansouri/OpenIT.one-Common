4864dbd2607d35caed44093da8520d308fbec39d
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-03 11:46:44 +0200

    Update paths.go

--- utils/constants/paths.go
+++ utils/constants/paths.go
@@ -1,1 +1,9 @@
+package constants
 
+const (
+	DefaultConfigPath  = "/etc/openit"
+	DefaultDataPath    = "/var/lib/openit"
+	DefaultFilePath    = "/var/lib/openit/files"
+	DefaultRuntimePath = "/var/run/openit"
+	DefaultLogPath     = "/var/log/openit"
+)
