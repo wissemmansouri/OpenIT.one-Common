d66c24ed579cd5027a13e5727c668a4971b9bfc7
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-17 12:15:48 +0200

    Create common.go

---
+++ external/common.go
@@ -0,0 +1,33 @@
+package external
+
+import (
+	"errors"
+	"net/http"
+	"os"
+	"time"
+
+	http2 "github.com/wisemmansouri/OpenIT.one-Common/utils/http"
+)
+
+func getAddress(addressFile string) (string, error) {
+	buf, err := os.ReadFile(addressFile)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buf), nil
+}
+
+func ping(address string, timeout time.Duration) error {
+	response, err := http2.Get(address+"/ping", timeout)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return errors.New("failed to ping the service as address " + address)
+	}
+
+	return nil
+}
